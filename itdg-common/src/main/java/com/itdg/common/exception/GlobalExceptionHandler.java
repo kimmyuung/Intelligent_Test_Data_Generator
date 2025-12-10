8e29eeefa2ccca4a430ff4e98da2e19670027197
김명호 <kimmyungho1003@naver.com>
2025-12-10 17:14:13 +0900

    멀티 모듈 셋팅 작업

---
+++ itdg-common/src/main/java/com/itdg/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.itdg.common.exception;
+
+import com.itdg.common.dto.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ApiResponse> handleBusinessException(BusinessException e) {
+        log.error("Business Exception: {}", e.getMessage(), e);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleException(Exception e) {
+        log.error("Unexpected Exception: {}", e.getMessage(), e);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("Internal server error occurred"));
+    }
+}
